19a87c678601bd1ae902b80d25225fc1a8182521
Antonis Sgouros <antonis@askanna.io>
2019-12-06 12:02:40 +0100

    [IMP] - connect the cookiecutter to the createproject

--- askanna_cli/createproject.py
+++ askanna_cli/createproject.py
@@ -1,5 +1,9 @@
 import click
+from cookiecutter.main import cookiecutter
 
+# FIXME: this will be an AskAnna public link in the future
+# For dev purposes, add your local path to the cookiecutter template
+ASKANNA_BASEDSP = "https://gitlab.askanna.io/askanna/cookiecutter-askannabasedsp.git"
 
 HELP = """
 This command will allow you to create a project
@@ -9,5 +13,9 @@ SHORT_HELP = "Create an AskAnna project"
 
 
 @click.command(help=HELP, short_help=SHORT_HELP)
-def cli():
+@click.argument("template", required=False, default="basedsp")
+def cli(template):
     click.echo("This is performing the createproject action")
+
+    if template and template == "basedsp" or "base":
+        cookiecutter(ASKANNA_BASEDSP)
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'Click>=7.0',
+    'cookiecutter>=1.6.0',
 ]
 
 setup_requirements = []
