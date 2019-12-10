76012a78b553538830f30b3aba57a5b4483c2869
Anthony Leung <anthony@askanna.io>
2019-12-10 11:50:38 +0100

    [TST] - #6 - Add initial test to the cli tests

---
+++ tests/base.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from click.testing import CliRunner
+
+from askanna_cli.tool import cli
+
+
+class BaseCLItest(unittest.TestCase):
+    verb = ""
+
+    def setUp(self):
+        self.result = CliRunner().invoke(cli, self.verb)
+
+    def testCommandLineAccess(self):
+        assert self.result.exit_code == 0
+        assert len(self.result.output) > 0
---
+++ tests/test_cli_push.py
@@ -0,0 +1,18 @@
+import pytest
+import unittest
+
+from .base import BaseCLItest
+
+
+class TestCliLogin(BaseCLItest):
+    """
+    askanna push
+
+    We expect to initiate a push action of our code to the askanna server
+    """
+    verb = "push"
+
+    def testCommandLoginBase(self):
+        assert "push" in self.result.output
+        self.assertIn("push", self.result.output)
+        self.assertNotIn("noop", self.result.output)
