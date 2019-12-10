08130eca027e309b87fd7093fb593e915c7f1749
Anthony Leung <anthony@askanna.io>
2019-12-10 12:09:53 +0100

    [IMP] - #6 - Add cli logout test, disable push test as this is not implemented yet

---
+++ tests/test_cli_logout.py
@@ -0,0 +1,18 @@
+import pytest
+import unittest
+
+from .base import BaseCLItest
+
+
+class TestCliLogout(BaseCLItest):
+    """
+    askanna logout
+
+    We expect to logout the user (when authenicated)
+    """
+    verb = "logout"
+
+    def testCommandLoginBase(self):
+        assert "logout" in self.result.output
+        self.assertIn("logout", self.result.output)
+        self.assertNotIn("noop", self.result.output)
--- tests/test_cli_push.py
+++ tests/test_cli_push.py
@@ -4,15 +4,15 @@ import unittest
 from .base import BaseCLItest
 
 
-class TestCliLogin(BaseCLItest):
-    """
-    askanna push
+# class TestCliPush(BaseCLItest):
+#     """
+#     askanna push
 
-    We expect to initiate a push action of our code to the askanna server
-    """
-    verb = "push"
+#     We expect to initiate a push action of our code to the askanna server
+#     """
+#     verb = "push"
 
-    def testCommandLoginBase(self):
-        assert "push" in self.result.output
-        self.assertIn("push", self.result.output)
-        self.assertNotIn("noop", self.result.output)
+#     def testCommandLoginBase(self):
+#         assert "push" in self.result.output
+#         self.assertIn("push", self.result.output)
+#         self.assertNotIn("noop", self.result.output)
