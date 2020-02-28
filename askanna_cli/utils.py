923192edfe92389e76acb9e4da98387f7d338988
Anthony Leung <anthony@askanna.io>
2020-02-28 13:21:39 +0100

    [IMP] - #20 - Add units

--- askanna_cli/utils.py
+++ askanna_cli/utils.py
@@ -1,7 +1,11 @@
 import os
 import glob
+import mimetypes
+import collections
 
 from pathlib import Path
+import zipfile
+from zipfile import ZipFile
 
 from yaml import load, dump
 try:
@@ -11,8 +15,14 @@ except ImportError:
 
 CONFIG_USERHOME_FILE = "~/.askanna.yml"
 
+StorageUnit = collections.namedtuple('StorageUnit', 
+[
+    'B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'
+])
+
+diskunit = StorageUnit(B=1, KiB=1024**1, MiB=1024**2, GiB=1024**3, TiB=1024**4, PiB=1024**5)
+
 def init_checks():
-    create_config(CONFIG_USERCONFIG_FILE)
     create_config(CONFIG_USERHOME_FILE)
 
 
@@ -68,3 +78,39 @@ def store_config(config):
     original_config.update(**config)
     output = dump(original_config, Dumper=Dumper) 
     return output
+
+
+
+# Zip the files from given directory that matches the filter
+def zipFilesInDir(dirName, zipFileName, filter):
+    os.chdir(dirName)
+    # create a ZipFile object
+    with ZipFile(zipFileName, mode='w') as zipObj:
+        # Iterate over all the files in directory
+        for folderName, subfolders, filenames in os.walk('.'):
+            for filename in filenames:
+                if filter(filename):
+                    # create complete filepath of file in directory
+                    filePath = os.path.join(folderName, filename)
+                    # Add file to zip
+                    zipObj.write(filePath)
+
+
+def _file_type(path):
+    """Mimic the type parameter of a JS File object.
+    Resumable.js uses the File object's type attribute to guess mime type,
+    which is guessed from file extention accoring to
+    https://developer.mozilla.org/en-US/docs/Web/API/File/type.
+    Parameters
+    ----------
+    path : str
+        The path to guess the mime type of
+    Returns
+    -------
+    str
+        The inferred mime type, or '' if none could be inferred
+    """
+    type_, _ = mimetypes.guess_type(path)
+    # When no type can be inferred, File.type returns an empty string
+    return '' if type_ is None else type_
+
