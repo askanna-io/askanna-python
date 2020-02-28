b13481aacbb74eeb88a183271d9f779a24a13ab0
Anthony Leung <anthony@askanna.io>
2020-02-28 14:29:12 +0100

    [IMP] - #22 - Change dsp to git

--- askanna_cli/createproject.py
+++ askanna_cli/createproject.py
@@ -4,6 +4,7 @@ from cookiecutter.main import cookiecutter
 # FIXME: this will be an AskAnna public link in the future
 # For dev purposes, add your local path to the cookiecutter template
 ASKANNA_BASEDSP = "https://gitlab.askanna.io/askanna/cookiecutter-askannabasedsp.git"
+ASKANNA_BASEDSP = "git@gitlab.askanna.io:askanna/cookiecutter-askannabasedsp.git"
 
 HELP = """
 This command will allow you to create a project
