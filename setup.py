87a77dea40b1a90de07ccb12830456714f89a550
Anthony Leung <anthony@askanna.io>
2020-06-05 09:30:24 +0200

    [IMP] - #41 - Upgrade version of cli

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://gitlab.askanna.io/askanna/askanna-cli',
-    version='0.1.0',
+    url='https://gitlab.askanna.io/open/askanna-cli',
+    version='0.2.0',
     zip_safe=False,
 )
