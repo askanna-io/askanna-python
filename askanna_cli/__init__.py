d801945559abb447ffe0c43c417569d2ff69c19b
Wai Yi Anthony Leung <w.y.leung@e-sensei.nl>
2020-10-24 18:55:23 +0200

    Fix installation loading appdirs

--- askanna_cli/__init__.py
+++ askanna_cli/__init__.py
@@ -6,7 +6,12 @@ __author__ = """Anthony Leung"""
 __email__ = 'anthony@askanna.io'
 __version__ = '0.3.1'
 
-from appdirs import AppDirs
-appname = "askanna"
-appauthor = "askanna"
-config_dirs = AppDirs(appname, appauthor)
+try:
+    from appdirs import AppDirs
+except:
+    # we are propably within an installation
+    pass
+else:
+    appname = "askanna"
+    appauthor = "askanna"
+    config_dirs = AppDirs(appname, appauthor)
